f5dee4bb076ac9f3488b9119066c24bb1ff7866a
Shely Jain <shelyndeli@gmail.com>
2025-12-04 22:16:57 -0800

    allow all cores

--- backend/server.js
+++ backend/server.js
@@ -17,20 +17,8 @@ const app = express();
 // ----------------------
 // CORS (FINAL VERSION)
 // ----------------------
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "http://localhost:5175",
-      "http://localhost:5176",
-      "https://high5-one.vercel.app",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
 
+app.use(cors());
 // JSON parsing
 app.use(express.json({ limit: "10mb" }));
 
